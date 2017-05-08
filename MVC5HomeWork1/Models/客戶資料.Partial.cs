21c2967df0ad548a21c61cf8576b4fcf4a0a5bc0
Calvin.egs <Calvin.egs@gmail.com>
2017-05-08 10:34:56 +0800

    add v1.0 all code

---
+++ MVC5HomeWork1/Models/客戶資料.Partial.cs
@@ -0,0 +1,45 @@
+namespace MVC5HomeWork1.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    
+    [MetadataType(typeof(客戶資料MetaData))]
+    public partial class 客戶資料
+    {
+    }
+    
+    public partial class 客戶資料MetaData
+    {
+        [Required]
+        public int Id { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        //[RegularExpression("[\u4e00-\u9fa5]")]
+        public string 客戶名稱 { get; set; }
+        
+        [StringLength(8, ErrorMessage="欄位長度不得大於 8 個字元")]
+        [Required]
+        public string 統一編號 { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        [Required]
+        [RegularExpression(@"[0-9]{2}\-[0-9]{8}", ErrorMessage = "電話格式不符 02-23456789")]
+        public string 電話 { get; set; }
+        
+        [StringLength(50, ErrorMessage="欄位長度不得大於 50 個字元")]
+        public string 傳真 { get; set; }
+        
+        [StringLength(100, ErrorMessage="欄位長度不得大於 100 個字元")]
+        public string 地址 { get; set; }
+        
+        [StringLength(250, ErrorMessage="欄位長度不得大於 250 個字元")]
+        [EmailAddress]
+        [Required]
+        public string Email { get; set; }
+    
+        public virtual ICollection<客戶銀行資訊> 客戶銀行資訊 { get; set; }
+        public virtual ICollection<客戶聯絡人> 客戶聯絡人 { get; set; }
+    }
+}
